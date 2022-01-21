c2e63e68580a31b7dc4a38f78eb74efca91f4726
Callum McIntyre <callum@pointer.gg>
2022-01-21 16:20:31 +0000

    Get user's packs on the /lounge page

--- pages/lounge.tsx
+++ pages/lounge.tsx
@@ -1,3 +1,9 @@
+import { useWeb3 } from "@3rdweb/hooks";
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import type { PackMetadataWithBalance } from "@3rdweb/sdk";
+import { useEffect, useState } from "react";
+import { packAddress } from "../lib/contractAddresses";
+
 export function getStaticProps() {
   return {
     props: {
@@ -7,5 +13,29 @@ export function getStaticProps() {
 }
 
 export default function Lounge() {
+  const { address } = useWeb3();
+  const [loading, setLoading] = useState(false);
+  const [packNfts, setPackNfts] = useState<PackMetadataWithBalance[]>([]);
+
+  const sdk = new ThirdwebSDK("https://rpc-mumbai.maticvigil.com");
+  const packModule = sdk.getPackModule(packAddress);
+
+  async function getNftsWithLoading() {
+    setLoading(true);
+    try {
+      const fetchedPackNfts = await packModule.getOwned(address);
+      console.log(fetchedPackNfts);
+      setPackNfts(fetchedPackNfts);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (address) {
+      getNftsWithLoading();
+    }
+  }, [address]);
+
   return <p>You need to own some NFTs to access the lounge!</p>;
 }
